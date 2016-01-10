ec276a879cce24cb464979d26b88fb8356517805
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2016-01-11 06:47:33 +0900

    ng-class adds readability to forecast page

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParam
 
 	$scope.city = cityService.city;
 
-	$scope.days = $routeParams.days || 2;
+	$scope.days = $routeParams.days || '2';
 
 	//use resource to reach out to the api
 	$scope.weatherAPI = 

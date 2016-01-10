f16123630be55dc175474e993fc959ca2f9d84cb
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2016-01-10 21:22:55 +0900

    making API call for data, and binding to the forecast page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Weather_Forecast_SPA
+
+This is a single page application that accepts a city name and returns the weather forecast for that city in the form of daily temperatures.
--- app.js
+++ app.js
@@ -16,6 +16,11 @@ weatherApp.config(function ($routeProvider) {
         controller: 'forecastController'
     })
 
+    .when('/forecast/:days', {
+        templateUrl: 'src/pages/forecast.htm',
+        controller: 'forecastController'
+    })
+
 });
 
 //SERVICES
@@ -36,8 +41,26 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function ($sco
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function ($scope, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function ($scope, $resource, $routeParams, cityService) {
 
 	$scope.city = cityService.city;
 
+	$scope.days = $routeParams.days || 2;
+
+	//use resource to reach out to the api
+	$scope.weatherAPI = 
+	$resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=1c4837379b96215d5c04282033857b31",  {
+		callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+
+	$scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+
+	//convert to Fareheit
+	$scope.convertToFahrenheit = function(degK) {
+		return Math.round((1.8 * (degK - 273)) + 32);
+	}
+
+	//convert the date to readable
+	$scope.convertToDate = function(dt) {
+		return new Date(dt * 1000);
+	}
 }]);
